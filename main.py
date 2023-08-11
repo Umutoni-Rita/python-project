fddd4820c7f4e682f302f8118d9d856e6528e0fb
Umutoni-Rita <umurita37@gmail.com>
2023-08-11 06:28:54 +0200

    Initialization

---
+++ main.py
@@ -0,0 +1,101 @@
+import sqlite3
+
+conn = sqlite3.connect('Students.db')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS students(
+            stId INTEGER PRIMARY KEY autoincrement, 
+            fname VARCHAR(100), lname VARCHAR(100),
+            school VARCHAR(100), grade VARCHAR(10))''')
+
+print("Welcome to Student Management System!! Choose your option to continue")
+def gradeStudent(grade_v):
+    if (grade_v <= 100 and grade_v >= 90):
+        grade = 'A'
+    elif(grade_v <= 89 and grade_v >= 75):
+        grade = 'B'
+    elif(grade_v <= 74 and grade_v >= 60):
+        grade = 'C'
+    elif(grade_v <= 59 and grade_v >= 50):
+        grade = 'D'
+    elif(grade_v <= 49 and grade_v >= 35):
+        grade = 'F'
+    else:
+        grade = 'F--'
+    return grade
+
+def createStudent(): 
+    fname = input("Enter your first name: ")
+    lname = input("Enter your last name: ")
+    school = input("Enter your school: ")
+    grade_value = int(input("Enter your marks: "))
+
+    grade = gradeStudent(grade_value)
+
+    cur.execute('''INSERT INTO students 
+                (fname, lname, school, grade) 
+                VALUES
+                (?, ?, ?, ?)''', (fname, lname, school, grade))
+    print("Your grade is ", grade)
+
+    conn.commit()
+    cur.close
+
+def viewStudents():
+    data = cur.execute("SELECT * FROM students ORDER BY grade ASC")
+    for row in data:
+        print(row)
+
+while True:
+    print("1. Create a student \n2. View list students \n3. Update a student \n4. Delete a student \n5. Exit")
+    option = input("Your option: ")
+
+    if option == '1':
+        createStudent()
+    elif option == '2':
+        viewStudents()
+    elif option == '3':
+        print("Coming soon")
+    elif option == '4':
+        print("Coming soon")
+    elif option == '5':
+        break
+    else:
+        print("Invalid option")
+
+cur.close()
+
+
+
+# class Users:
+#     def __init__(self, fname, lname, school, grade) :
+#         self.fname = fname
+#         self.lname = lname
+#         self.school = school
+#         self.grade = grade
+
+#     def gradeStudent(self):
+#         grade = ''
+#         if (self.grade <= 100 and self.grade >= 90):
+#             grade = 'A'
+#         elif(self.grade <= 89 and self.grade >= 75):
+#             grade = 'B'
+#         elif(self.grade <= 74 and self.grade >= 60):
+#             grade = 'C'
+#         elif(self.grade <= 59 and self.grade >= 50):
+#             grade = 'D'
+#         elif(self.grade <= 49 and self.grade >= 35):
+#             grade = 'F'
+#         else:
+#             grade = 'Get out of school'
+#         print(grade) 
+    
+# fname = input("Enter your name: ")
+# lname = input("Enter your last name: ")
+# school = input("Enter your school: ")
+# grade = int(input("Enter your grade: "))
+
+# student = Users({fname}, {lname}, {school}, {grade})
+# student.gradeStudent()
+
+
